887ac77aae8043f68efd35a79d33a93b420ee4a3
银河灿灿 <65111206+lifeafter619@users.noreply.github.com>
2021-10-31 11:23:46 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,14 +1,10 @@
 
 /*
 
-  Shape Shifter
-  =============
-  A canvas experiment by Kenneth Cachia
-  http://www.kennethcachia.com
 
   Updated code
   ------------
-  https://github.com/kennethcachia/Shape-Shifter
+  https://github.com/lifeafter619
 
 */
 
@@ -24,7 +20,7 @@ var S = {
     if (i !== -1) {
       S.UI.simulate(decodeURI(action).substring(i + 3));
     } else {
-      S.UI.simulate('|#countdown 3||某|某|祝|你|生|日|快|乐|#rectangle|');
+      S.UI.simulate('|#countdown 3|祝|你|生|日|快|乐|#rectangle|');
     }
 
     S.Drawing.loop(function () {
