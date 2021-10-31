1b38990d928933c66f44e897b13f8ca5009ed086
银河灿灿 <65111206+lifeafter619@users.noreply.github.com>
2021-10-31 11:32:06 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -20,7 +20,7 @@ var S = {
     if (i !== -1) {
       S.UI.simulate(decodeURI(action).substring(i + 3));
     } else {
-      S.UI.simulate('|#countdown 3|祝|你|生|日|快|乐|#rectangle|');
+      S.UI.simulate('|#countdown 3||某|某|祝|你|生|日|快|乐|XX生日快乐');
     }
 
     S.Drawing.loop(function () {
